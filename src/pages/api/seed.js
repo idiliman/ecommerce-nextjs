59115a06376c9ef6ebb3e1f5c86e7420009f9abe
unknown <idil.iman1999@gmail.com>
2023-02-13 12:18:21 +0800

    Seed products to DB

--- src/pages/api/seed.js
+++ src/pages/api/seed.js
@@ -1,10 +1,22 @@
+import Product from '@/models/Product';
 const { default: User } = require('@/models/User');
 const { default: data } = require('@/utils/data');
 const { default: db } = require('@/utils/db');
 
+// mongosh "mongodb+srv://cluster0.soub0b9.mongodb.net/myFirstDatabase" --apiVersion 1 --username admin
+
+// Seed user to DB
+// const handler = async (req, res) => {
+//   await db.connect();
+//   await User.insertMany(data.users);
+//   await db.disconnect();
+//   res.send({ message: 'Seeded successfully' });
+// };
+
+// Seed product to DB
 const handler = async (req, res) => {
   await db.connect();
-  await User.insertMany(data.users);
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'Seeded successfully' });
 };
